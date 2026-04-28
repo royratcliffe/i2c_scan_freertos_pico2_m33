8866fc3bf203ca41d58753143f40ab2b68080153
Roy Ratcliffe <roy@ratcliffe.me>
2026-04-28 15:44:16 +0100

    Add stack overflow hook implementation for FreeRTOS
    Implement the vApplicationStackOverflowHook function to handle
    stack overflow detection in FreeRTOS. This includes disabling
    interrupts and asserting a failure condition.

---
+++ app_stack_overflow.c
@@ -0,0 +1,20 @@
+#include "FreeRTOS.h"
+#include "task.h"
+
+/*
+ * Run time stack overflow checking is performed if
+ * configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2. This hook
+ * function is called if a stack overflow is detected.
+ */
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
+  (void)xTask;
+  (void)pcTaskName;
+
+  /*
+   * Force an assert.
+   */
+  taskDISABLE_INTERRUPTS();
+  configASSERT((volatile void *)NULL);
+  for (;;)
+    ;
+}
